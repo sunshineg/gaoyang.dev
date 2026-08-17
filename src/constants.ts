303fd25c1d59e499489107be583ba34f0b9cbe5b
Sunny <1147886+sunshineg@users.noreply.github.com>
2026-08-17 19:28:53 +0800

    feat: homepage hero avatar, footer/share links polish (#3)
    - Hero: horizontal layout with circular avatar (avatar-home.png, conditional render)
    - Remove Recent Posts heading on homepage
    - Remove BackButton and disable Edit page on post details
    - Footer: single line Copyright | CC BY 4.0 | Built With MindMux
    - Update X social link to x.com/gaoyangio
    - Share links: X, LinkedIn, Facebook, Telegram, Email (drop WhatsApp/Pinterest)

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,8 @@ import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
-import IconPinterest from "@/assets/icons/IconPinterest.svg";
 
 interface Social {
   name: string;
@@ -24,7 +22,7 @@ export const SOCIALS: Social[] = [
   },
   {
     name: "X",
-    href: "https://twitter.com/sunshineg",
+    href: "https://x.com/gaoyangio",
     linkTitle: `Sunny on X`,
     icon: IconBrandX,
   },
@@ -44,10 +42,16 @@ export const SOCIALS: Social[] = [
 
 export const SHARE_LINKS: Social[] = [
   {
-    name: "WhatsApp",
-    href: "https://wa.me/?text=",
-    linkTitle: `Share this post via WhatsApp`,
-    icon: IconWhatsapp,
+    name: "X",
+    href: "https://x.com/intent/post?url=",
+    linkTitle: `Share this post on X`,
+    icon: IconBrandX,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/sharing/share-offsite/?url=",
+    linkTitle: `Share this post on LinkedIn`,
+    icon: IconLinkedin,
   },
   {
     name: "Facebook",
@@ -56,25 +60,13 @@ export const SHARE_LINKS: Social[] = [
     icon: IconFacebook,
   },
   {
-    name: "X",
-    href: "https://x.com/intent/post?url=",
-    linkTitle: `Share this post on X`,
-    icon: IconBrandX,
-  },
-  {
     name: "Telegram",
     href: "https://t.me/share/url?url=",
     linkTitle: `Share this post via Telegram`,
     icon: IconTelegram,
   },
   {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
-    icon: IconPinterest,
-  },
-  {
-    name: "Mail",
+    name: "Email",
     href: "mailto:?subject=See%20this%20post&body=",
     linkTitle: `Share this post via email`,
     icon: IconMail,
